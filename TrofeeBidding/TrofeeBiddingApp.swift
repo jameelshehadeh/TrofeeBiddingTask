9ea9f56092bf10a57da7badc289c08e9f956c990
Jameel Shehadeh <jamshehadeh@gmail.com>
2025-08-15 14:52:00 +0300

    Initial Commit

---
+++ TrofeeBidding/TrofeeBiddingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrofeeBiddingApp.swift
+//  TrofeeBidding
+//
+//  Created by Jameel Shehadeh on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrofeeBiddingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
